fc32636c95450659681f7552dc460f105316a2fe
kedam6 <kedam6@gmail.com>
2014-05-14 15:41:36 +0200

    v0.1.3
    Doge, richtextbox

--- Nigga/Nigga/MainWindow.xaml.cs
+++ Nigga/Nigga/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace IO_Projekt
 {
@@ -23,6 +25,9 @@ namespace IO_Projekt
         public MainWindow()
         {
             InitializeComponent();
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+
         }
 
         private void exitMenuItem_Click(object sender, RoutedEventArgs e)
@@ -32,46 +37,70 @@ namespace IO_Projekt
 
         private void saveMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-            dlg.FileName = "Document"; // Default file name
-            dlg.DefaultExt = ".text"; // Default file extension
-            dlg.Filter = "Text document (.txt)|*.txt"; // Filter files by extension
 
-            // Show save file dialog box
-            Nullable<bool> result = dlg.ShowDialog();
-
-            // Process save file dialog box results
-            if (result == true)
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
             {
-                // Save document
-                System.IO.File.WriteAllText(dlg.FileName, editSpace.Text);
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+                TextRange range = new TextRange(editSpace.Document.ContentStart, editSpace.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
             }
+
         }
 
         private void loadMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-            dlg.FileName = "Example";
-            dlg.DefaultExt = ".txt";
-            dlg.Filter = "Text document (.txt)|*.txt";
-
-            Nullable<bool> result = dlg.ShowDialog();
-
-            if(result == true)
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
             {
-                editSpace.Text = System.IO.File.ReadAllText(dlg.FileName);
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+                TextRange range = new TextRange(editSpace.Document.ContentStart, editSpace.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
             }
+
         }
 
         private void aboutMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            Nigga.AboutWindow aboutWin = new Nigga.AboutWindow();
+            var aboutWin = new Nigga.AboutWindow();
             aboutWin.ShowDialog();
         }
 
         private void newMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            editSpace.Text = "";
+            editSpace.Document = new FlowDocument();
         }
+
+        private void editSpace_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            object temp = editSpace.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+            temp = editSpace.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+            temp = editSpace.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+            temp = editSpace.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            cmbFontFamily.SelectedItem = temp;
+            temp = editSpace.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            cmbFontSize.Text = temp.ToString();
+
+        }
+
+        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFontFamily.SelectedItem != null)
+                editSpace.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+
+        }
+
+        private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            editSpace.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+        }
+
+
     }
 }
